d58fc8d6af54c350bd85847bf9dde722cbcb0a62
Selvasundar Kumar <selvasundar1993@gmail.com>
2021-12-29 11:27:53 +0530

    Add files via upload

---
+++ NodeMCU_LED_Control_STA_Mode.ino
@@ -0,0 +1,83 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+
+/*Put your SSID & Password*/
+const char* ssid = "SSID-Name";  // Enter SSID here
+const char* password = "Password";  //Enter Password here
+
+ESP8266WebServer server(80);
+
+uint8_t LEDpin = D1;
+bool LEDstatus = LOW;
+
+void setup() {
+  Serial.begin(115200);
+  delay(100);
+  pinMode(LEDpin, OUTPUT);
+
+  Serial.println("Connecting to ");
+  Serial.println(ssid);
+
+  //connect to your local wi-fi network
+  WiFi.begin(ssid, password);
+
+  //check wi-fi is connected to wi-fi network
+  while (WiFi.status() != WL_CONNECTED) 
+  {
+  delay(1000);
+  Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected..!");
+  Serial.print("Got IP: ");  
+  Serial.println(WiFi.localIP());
+
+  server.on("/", handle_OnConnect);
+  server.on("/ledon", handle_ledon);
+  server.on("/ledoff", handle_ledoff);
+  server.onNotFound(handle_NotFound);
+
+  server.begin();
+  Serial.println("HTTP server started");
+}
+void loop() {
+  server.handleClient();
+  digitalWrite(LEDpin, LEDstatus);
+}
+
+void handle_OnConnect() {
+  LEDstatus = LOW;
+  server.send(200, "text/html", SendHTML()); 
+}
+
+void handle_ledon() {
+  LEDstatus = HIGH;
+  server.send(200, "text/html", SendHTML()); 
+}
+
+void handle_ledoff() {
+  LEDstatus = LOW;
+  server.send(200, "text/html", SendHTML()); 
+}
+
+void handle_NotFound(){
+  server.send(404, "text/plain", "Not found");
+}
+
+String SendHTML(){
+  String ptr = "<!DOCTYPE html>\n";
+  ptr +="<html>\n";
+  ptr +="<head>\n";
+  ptr +="<title>LED Control</title>\n";
+  ptr +="</head>\n";
+  ptr +="<body>\n";
+  ptr +="<h1>LED Control using WiFi</h1>\n";
+  ptr +="<p>Click to switch LED on and off.</p>\n";
+  ptr +="<form method=\"get\">\n";
+  ptr +="<input type=\"button\" value=\"LED ON\" onclick=\"window.location.href='/ledon'\">\n";
+  ptr +="<input type=\"button\" value=\"LED OFF\" onclick=\"window.location.href='/ledoff'\">\n";
+  ptr +="</form>\n";
+  ptr +="</body>\n";
+  ptr +="</html>\n";
+  return ptr;
+}
